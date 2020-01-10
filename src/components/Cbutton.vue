ab13ef1b4b94d4ce22216adb5a01ecdf7c8b9584
xiaoYu <18839199638@163.com>
2020-01-10 21:21:01 +0800

    按钮组件初步封装完毕

---
+++ src/components/Cbutton.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="btn" :class="[size,type]">
+    <span class="iconfont" :class="icon"></span>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: 'large'
+    },
+    icon: {
+      type: String
+    },
+    type: {
+      type: String,
+      default: 'primary'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.btn {
+  margin: 0 auto;
+  text-align: center;
+  color: #fff;
+}
+.large {
+  width: 260/360*100vw;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 20px;
+}
+.small {
+  width: 75/360*100vw;
+  height: 30px;
+  line-height: 30px;
+  border-radius: 15px;
+}
+.primary {
+  background-color: #1989fa;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>登录页</div>
+  <div>登录页
+    <cbutton>登录</cbutton>
+    <cbutton size="small">登录</cbutton>
+  </div>
 </template>
 
 <script>
+import cbutton from '@/components/Cbutton.vue'
 export default {
-
+  components: {
+    cbutton
+  }
 }
 </script>
 
